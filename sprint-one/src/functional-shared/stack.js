c0a2ffa91c97c5784ec480ce252d4e955977b601
brachbach <brachbach@gmail.com>
2016-04-20 15:25:51 -0700

    function shared stack complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.length = 0;
+  newStack.data = {};
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.length;
+};
 
+stackMethods.push = function(value) {
+  this.data[this.length++] = value;
+};
+
+stackMethods.pop = function(value) {
+  if (this.length > 0) {
+    var popped = this.data[this.length-1];
+    this.length--;
+    return popped;
+  }
+
+  return null;
+};
