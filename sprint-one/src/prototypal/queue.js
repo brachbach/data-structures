15a70c12a4f4742271fe8ea5c07454779d75f39f
brachbach <brachbach@gmail.com>
2016-04-20 15:53:16 -0700

    Implemented prototypal queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.end = 0;
+  newQueue.front = 0;
+  newQueue.data = {};
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.end - this.front;
+};
+
+queueMethods.enqueue = function(value) {
+  this.data[this.end] = value;
+  this.end++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.size() > 0) {
+    var dequeued = this.data[this.front];
+    this.front++;
+    return dequeued;
+  }
+  return null;
+};
+
 
 
