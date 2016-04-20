aea1780f4c716a44f110618a20072737115cbafe
Sean Ho <seanthaoho@gmail.com>
2016-04-20 12:12:28 -0700

    finished stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.length++;
+    storage[storage.length] = value;
   };
 
   someInstance.pop = function() {
+    if(storage.length > 0) {
+      storage.length--;
+      return storage[storage.length + 1];
+    }
+
+    return 0;
   };
 
   someInstance.size = function() {
+    return storage.length;
   };
 
   return someInstance;
