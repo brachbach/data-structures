bd4b7abea9fb8dea86994844f9f3b8584c80c4d8
brachbach <brachbach@gmail.com>
2016-04-21 09:57:45 -0700

    tree completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,9 +11,21 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 
