a7089b4f649c887089c69c2424a9fd043f61c751
brachbach <brachbach@gmail.com>
2016-04-21 16:41:02 -0700

    working on hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var element = this.retrieve(index);
+  // if (v === 'val1') {
+  //   debugger;
+  // }
+  if (!element) {
+    var newObject = {};
+    newObject[k] = v;
+    this._storage.set(index, newObject);
+  } else {
+    element[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (k === 'val2') {
+    debugger;
+  }
+  return this._storage.get(index) ? this._storage.get(index)[k] : undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
