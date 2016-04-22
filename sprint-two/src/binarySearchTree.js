92d9e9a18577673d93609634debfbd6225ea5eb8
brachbach <brachbach@gmail.com>
2016-04-21 20:53:22 -0700

    implemented binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,46 @@
 var BinarySearchTree = function(value) {
+  var newBST = {};
+  newBST.left = null;
+  newBST.right = null;
+  newBST.value = value;
+
+  newBST.getSide = function(value) {
+    var result = (value < this.value ? 'left' : 'right');
+    return result;
+  };
+
+  newBST.insert = function (value) {
+    var side = this.getSide(value);
+    if (this[side]) {
+      this[side].insert(value);
+    } else {
+      this[side] = BinarySearchTree(value);
+    }
+  };
+
+  newBST.contains = function (value) {
+    if (this.value === value) {
+      return true;
+    }
+    var side = this.getSide(value);
+    if (this[side]) {
+      return this[side].contains(value);
+    } else {
+      return false;
+    }
+  };
+  newBST.depthFirstLog = function (callback) {
+    callback(this.value);
+    if (this.left) {
+      this.left.depthFirstLog(callback);
+    }
+
+    if (this.right) {
+      this.right.depthFirstLog(callback);
+    }
+  };
+
+  return newBST;
 };
 
 
