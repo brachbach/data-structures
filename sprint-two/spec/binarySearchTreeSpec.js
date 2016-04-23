5e5b9c500a392b895ecd0398f2d8f7ba9dd93a1d
brachbach <brachbach@gmail.com>
2016-04-22 20:47:30 -0700

    did some stuff

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -12,12 +12,12 @@ describe('binarySearchTree', function() {
   });
 
   it('should insert values at the correct location in the tree', function() {
-    binarySearchTree.insert(2);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
-    binarySearchTree.insert(6);
-    expect(binarySearchTree.left.right.value).to.equal(3);
-    expect(binarySearchTree.right.left.value).to.equal(6);
+    expect(binarySearchTree.left.left.value).to.equal(3);
+    expect(binarySearchTree.right.right.value).to.equal(7); //Changed to account for automatic rebalancing
   });
 
   it('should have a working "contains" method', function() {
@@ -31,9 +31,69 @@ describe('binarySearchTree', function() {
   it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
     var array = [];
     var func = function(value) { array.push(value); };
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    binarySearchTree.depthFirstLog(func);
+    expect(array).to.eql([5, 4, 6]);
+  });
+
+  it('should have methods named maxHeight, minHeight, and rebalance', function() {
+    expect(binarySearchTree.maxHeight).to.be.a('function');
+    expect(binarySearchTree.minHeight).to.be.a('function');
+    expect(binarySearchTree.rebalance).to.be.a('function');
+
+  });
+
+  it('should have a working maxHeight', function() {
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
-    binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([5, 2, 3]);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    expect(binarySearchTree.maxHeight()).to.equal(4);
   });
+
+  it('should have a working minHeight', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    expect(binarySearchTree.minHeight()).to.equal(2);
+  });
+
+  it('should have a working rebalance', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    binarySearchTree.rebalance();
+    expect(binarySearchTree.maxHeight()).to.equal(3);
+  });
+
+  it('should rebalance automatically when maxHeight > (minHeight *2)', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(10);
+    expect(binarySearchTree.maxHeight()).to.equal(4);
+  });
+
+  it('should have working bfs', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(10);
+    expect(binarySearchTree.bfs()).to.equal([]);
+  });
+
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,12 +9,22 @@ var BinarySearchTree = function(value) {
     return result;
   };
 
-  newBST.insert = function (value) {
+  newBST.insertHelper = function(value) {
     var side = this.getSide(value);
     if (this[side]) {
       this[side].insert(value);
     } else {
       this[side] = BinarySearchTree(value);
+    }
+  };
+
+
+  newBST.insert = function (value) {
+
+    this.insertHelper(value);
+
+    if (this.minHeight() * 2 < this.maxHeight()) {
+      this.rebalance();
     }
   };
 
@@ -29,6 +39,7 @@ var BinarySearchTree = function(value) {
       return false;
     }
   };
+
   newBST.depthFirstLog = function (callback) {
     callback(this.value);
     if (this.left) {
@@ -40,6 +51,108 @@ var BinarySearchTree = function(value) {
     }
   };
 
+  newBST.depthFirstEach = function (callback) {
+    if (this.left) {
+      this.left.depthFirstEach(callback);
+    }
+
+    callback(this);
+
+    if (this.right) {
+      this.right.depthFirstEach(callback);
+    }
+
+  };
+
+  newBST.minHeight = function() {
+    var minHeightHelper = function(tree, count) {
+
+      var leftHeight = count;
+      var rightHeight = count;
+      // debugger;
+      if (tree.left) {
+        leftHeight = minHeightHelper(tree.left, count + 1);
+      }
+      if (tree.right) {
+        rightHeight = minHeightHelper(tree.right, count + 1);
+      }
+      return Math.min(leftHeight, rightHeight);
+    };
+    return minHeightHelper(this, 1);
+  };
+
+  newBST.maxHeight = function() {
+    var maxHeightHelper = function(tree, count) {
+
+      var leftHeight = count;
+      var rightHeight = count;
+      // debugger;
+      if (tree.left) {
+        leftHeight = maxHeightHelper(tree.left, count + 1);
+      }
+      if (tree.right) {
+        rightHeight = maxHeightHelper(tree.right, count + 1);
+      }
+      return Math.max(leftHeight, rightHeight);
+    };
+    return maxHeightHelper(this, 1);
+  };
+
+  newBST.rebalance = function() {
+  
+    var nodeArray = [];
+    var middle;
+    this.depthFirstEach(function(node) {
+      nodeArray.push(node.value);
+    });
+
+    middle = Math.floor(nodeArray.length / 2);
+
+    this.value = nodeArray[middle];
+    this.left = null;
+    this.right = null;
+
+    leftHalf = nodeArray.slice(0, middle);
+    rightHalf = nodeArray.slice(middle + 1);
+    this.recursiveInsertion(leftHalf);
+    this.recursiveInsertion(rightHalf);
+  };
+
+  newBST.recursiveInsertion = function (array) {
+    middle = Math.floor(array.length / 2);
+    this.insertHelper(array[middle]);
+    leftHalf = array.slice(0, middle);
+    rightHalf = array.slice(middle + 1);
+
+    if (leftHalf.length > 0) {
+      this.recursiveInsertion(leftHalf);
+    }
+
+    if (rightHalf.length > 0) {
+      this.recursiveInsertion(rightHalf);
+    }
+  };
+
+  newBST.bfs = function () {
+    var array = [];
+    var search = function(value) {
+      // var output;
+      // depthFirstEach(function(node) {
+      //   if (value === node.value) {
+      //     return node;
+      //   }
+      // });
+
+      // return output;
+    };
+
+    var bfsHelper = function(tree) {
+      array.push(tree.value);
+      bfsHelper(tree.left);
+      bfsHelper(tree.right);
+    };
+
+  };
   return newBST;
 };
 
